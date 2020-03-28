21dfa3031984d54b8845817aab984a73d5aef152
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-03-29 00:13:48 +0300

    navbar dropdown card created

---
+++ src/components/Layout/DropdownCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="dropdownCard">
+    <v-card shaped class="py-0 my-0" :color="backgroundColor">
+      <v-card-title>
+        <v-row
+          justify="center"
+          align="center"
+        >
+          <v-col cols="12" class="my-0 py-0">
+            <h1 class="subtitle-1 text-center secondary--text">{{ name }}</h1>
+          </v-col>
+          <v-col cols="8">
+            <v-divider dark></v-divider>
+          </v-col>
+        </v-row>
+      </v-card-title>
+      <v-card-text>
+        <v-list nav shaped>
+          <v-list-item
+            color="secondary"
+            selectable
+            :to="lesson.route"
+            link
+            v-for="lesson in lessons"
+            :key="lesson.name"
+          >
+            {{ lesson.name }}
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    lessons: Array,
+  },
+  data() {
+    return {
+      backgroundColor: 'rgb(63,81,181, 0.7)',
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
