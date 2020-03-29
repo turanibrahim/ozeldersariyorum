1be9d9dcca009e6b674c021153fe3ea99c61f95c
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-03-29 22:10:48 +0300

    added subtitle and bug fix

--- src/components/Home/CategoryLessons.vue
+++ src/components/Home/CategoryLessons.vue
@@ -6,30 +6,40 @@
       dense
     >
       <v-col cols="12">
-        <v-chip
-          outlined
-          link
-          large
-          label
-          :color="chipColor"
-          to="/about"
-        >
-          {{ title }}
-        </v-chip>
-        <v-divider class="mt-2"/>
+        <v-row>
+          <v-col cols="auto" class="py-0 pr-0 my-0" align-self="center">
+            <v-chip
+              outlined
+              link
+              label
+              :color="chipColor"
+              to="/about"
+            >
+              {{ title }}
+            </v-chip>
+          </v-col>
+          <v-col cols="auto">
+            <v-divider vertical></v-divider>
+          </v-col>
+          <v-col cols="auto" class="pl-0">
+            <h1 class="subtitle-1">{{ subtitle }}</h1>
+          </v-col>
+        </v-row>
       </v-col>
       <v-col cols="2" v-for="lesson in lessons" :key="lesson.id">
         <v-card
           class="mx-auto"
         >
           <v-img
-            height="250"
+            height="200"
             src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
             class="white--text align-end"
             gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.9)"
           >
 
-            <v-card-title class="py-0">{{ lesson.title }}</v-card-title>
+            <v-card-title class="py-0">
+              <h1 class="subtitle-1">{{ lesson.title }}</h1>
+            </v-card-title>
             <v-spacer/>
             <v-card-text class="py-0">
               <v-row
@@ -42,11 +52,11 @@
                   dense
                   half-increments
                   readonly
-                  size="14"
+                  size="12"
                   class="py-0"
                 ></v-rating>
 
-                <div class="ml-4 py-0 my-0">{{ lesson.stars }} {{ lesson.starCount }}</div>
+                <div class="ml-2 py-0 my-0">({{ lesson.starCount }})</div>
               </v-row>
 
               <div class="mb-4 subtitle-1 py-0">
@@ -66,6 +76,7 @@ export default {
     title: String,
     lessons: Array,
     chipColor: String,
+    subtitle: String,
   },
 };
 </script>
