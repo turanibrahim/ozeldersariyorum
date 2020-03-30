c2a25218caf4416b00d9a589073c7797e224d783
Ibrahim Turan <ibrahimturan002@gmail.com>
2020-03-31 00:37:31 +0300

    express server added for route dist folder on heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+
+const app = express();
+app.use(serveStatic(`${__dirname}/dist`));
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`server started ${port}`);
